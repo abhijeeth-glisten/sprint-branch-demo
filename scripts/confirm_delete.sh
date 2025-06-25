a16a1cbf2b14f99798714b1e3a7d788b8541a44f
bpatil <abhijeeth.glisten@gmail.com>
2025-06-25 15:48:49 +0530

    Update confirm_delete.sh

--- scripts/confirm_delete.sh
+++ scripts/confirm_delete.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu
 
-case "${CONFIRM_DELETE,,}" in
-  yes|true)
-    echo "Deletion confirmed."
-    ;;
-  *)
-    echo "Deletion not confirmed. Set 'confirm_delete' to YES or true."
-    exit 1
-    ;;
-esac
+CONFIRM_DELETE=${CONFIRM_DELETE:-NO}
+CONFIRM_DELETE_UPPER=$(echo "$CONFIRM_DELETE" | tr '[:lower:]' '[:upper:]')
+
+if [[ "$CONFIRM_DELETE_UPPER" == "YES" ]]; then
+  echo "Deletion confirmed."
+else
+  echo "Deletion not confirmed. Set 'confirm_delete' to YES."
+  exit 1
+fi
